933eed89b6378aa25dfd4c3a3b6f151d513bebfa
HaJunRyu <hjr4557@naver.com>
2022-06-16 11:21:27 +0900

    test: build test

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
-  <span>hellogfgggf</span>
 </template>
 
 <style>
